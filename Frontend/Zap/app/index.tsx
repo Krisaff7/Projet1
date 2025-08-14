ac0d30d846daeff6719d3fbfde3c2bcfc10a4338
Krisaff <affokponkris4@gmail.com>
2025-08-14 01:42:46 +0100

    debut

--- Frontend/Zap/app/index.tsx
+++ Frontend/Zap/app/index.tsx
@@ -1,9 +1,21 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-5 justify-center items-center">
-      <Text>Bye.</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello, World!</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems:'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+});
